6ae871b90d8fc120977bbd99eeddaa9f186b6e66
J0SE-S <joseph.simutis@icloud.com>
2023-05-07 17:25:20 -0400

    Added a whole bunch of stuff
    I added the Mirror Library and used it to add local AND remote player gameobjects.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,14 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class CharacterMovement : MonoBehaviour {
+public class CharacterMovement : NetworkBehaviour {
     [SerializeField] Rigidbody body;
+    [SerializeField] GameObject cameraManager;
     Vector3 moveDirection;
-    const float speed = 1f;
+    const float speed = 0.25f;
+    const float rotationSpeed = 10f;
 
-    void FixedUpdate() {
+    [Client]
+    void Start() {
+        if (!isOwned) return;
+        cameraManager = GameObject.Find("CameraManager");
+    }
+
+    [Client]
+    void Update() {
+        if (!isOwned) return;
+        cameraManager.transform.position = transform.position;
+
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        currentRotation.y += Input.mouseScrollDelta.y * rotationSpeed;
+        transform.rotation = Quaternion.Euler(currentRotation);
+
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed / 2;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed / 2;
+
+        /*Vector3 cameraRotation = cameraManager.transform.rotation.eulerAngles;
+        cameraRotation.z = 0;
+        cameraRotation.y -= Input.mouseScrollDelta.y * rotationSpeed;
+        cameraManager.transform.rotation = Quaternion.Euler(cameraRotation);*/
+
+        if (Input.GetMouseButton(1)) {
+            Vector3 cameraRotation = cameraManager.transform.rotation.eulerAngles;
+            cameraRotation.z = 0;
+
+            cameraRotation.x -= mouseY;
+            cameraRotation.y += mouseX;
+            cameraRotation.x = Mathf.Clamp(cameraRotation.x, 10, 90);
+
+            cameraManager.transform.rotation = Quaternion.Euler(cameraRotation);
+        }
+
         moveDirection = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
+    }
+
+    [Client]
+    void FixedUpdate() {
+        if (!isOwned) return;
+
         body.AddForce(moveDirection.normalized * speed, ForceMode.VelocityChange);
     }
 }
