ad4f91c7af8ea752753bc9751503adb09a65f667
J0SE-S <joseph.simutis@icloud.com>
2023-05-17 17:44:31 -0400

    Added bot prefab, does nothing yet

---
+++ Assets/Scripts/BotMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BotMovement : NetworkBehaviour {
+    [Server]
+    void Start() {
+        
+    }
+
+    [Server]
+    void Update() {
+        
+    }
+}
